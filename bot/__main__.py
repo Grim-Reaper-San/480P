c40fe1243f992d42679e82de61717fb8f67c72d9
Grim-Reaper-San <103260040+Grim-Reaper-San@users.noreply.github.com>
2022-07-08 14:49:09 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -86,6 +86,10 @@ async def _(e):
 @bot.on(events.NewMessage(pattern="/bash"))
 async def _(e):
     await bash(e)
+    
+@bot.on(events.NewMessage(pattern="/link"))
+async def _(e):
+    await dl_link(e)    
 
 
 ########## AUTO ###########
