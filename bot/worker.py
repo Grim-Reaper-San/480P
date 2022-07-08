c64956bef96c04fa8343089b9cbe07123a129c01
Grim-Reaper-San <103260040+Grim-Reaper-San@users.noreply.github.com>
2022-07-08 15:43:01 +0530

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -35,10 +35,6 @@ async def dl_link(event):
         pass
     if not link:
         return
-    if WORKING or QUEUE:
-        QUEUE.update({link: name})
-        return await event.reply(f"Added {link} in QUEUE")
-    WORKING.append(1)
     s = dt.now()
     xxx = await event.reply("`Downloading from url...`")
     try:
